163005576150d628dc038bbedf439c5a6733be33
Amandine <amandine.watrelos@gmail.com>
2022-06-25 22:47:59 +0900

    Initial commit

---
+++ src/main/java/awts/perso/music_festival_api/MusicFestivalApplication.java
@@ -0,0 +1,13 @@
+package awts.perso.music_festival_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicFestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicFestivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/awts/perso/music_festival_api/MusicFestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package awts.perso.music_festival_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicFestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
